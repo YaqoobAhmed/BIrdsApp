3fc134c558fad44a55ec54883e0041cd10d0c5ab
Yaqoob Ahmed <98280210+YaqoobAhmed@users.noreply.github.com>
2024-03-10 04:43:51 +0500

    final design of drawer

--- lib/Views/Onboarding/Screen/onboarding.dart
+++ lib/Views/Onboarding/Screen/onboarding.dart
@@ -1,4 +1,6 @@
+import 'package:firebase/Views/Login/login.dart';
 import 'package:firebase/Views/Onboarding/Widget/onboard_container.dart';
+import 'package:firebase/Views/Profile/Screen/Profile_screen.dart';
 import 'package:firebase/colors.dart';
 import 'package:firebase/navBar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,91 +19,77 @@ class _NavigationDrawerState extends State<NavigationDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
-          } else {
-            User? currentUser = snapshot.data;
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                buildHeader(context, currentUser),
-                buildMenuItems(context),
-              ],
-            );
-          }
-        },
-      ),
-    );
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        buildHeader(context),
+        buildMenuItems(context),
+      ],
+    ));
   }
 }
 
-Widget buildHeader(BuildContext context, User? user) => Container(
-      height: MediaQuery.of(context).size.height * 0.2,
-      width: MediaQuery.of(context).size.width * 1,
-      color: Colors.blue,
-      padding:
-          EdgeInsets.only(top: MediaQuery.of(context).padding.top, bottom: 24),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            user?.displayName ?? 'No Name',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            user?.email ?? '',
-            style: TextStyle(color: Colors.white),
-          ),
-        ],
+Widget buildHeader(BuildContext context) => GestureDetector(
+      onTap: () => Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProfileScreen(),
+          )),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.2,
+        width: MediaQuery.of(context).size.width * 1,
+        color: blueColor,
+        padding: EdgeInsets.only(
+            top: MediaQuery.of(context).padding.top, bottom: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.black,
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey.shade300,
+                  radius: 38,
+                  child: Center(
+                    child: Icon(
+                      Icons.person,
+                      size: 60,
+                      color: blueColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              currentUser.email ?? 'Not Available',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
       ),
     );
+
 Widget buildMenuItems(BuildContext context) => SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
             ListTile(
-              onTap: () {
-                Navigator.push(
+              onTap: () => Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => BottomNavBar(initialTabIndex: 0),
-                  ),
-                );
-              },
-              leading: Icon(
-                Icons.shopping_bag,
-              ),
-              title: Text("Buy"),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(Icons.sell),
-              title: Text("Sell"),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(Icons.camera_alt),
-              title: Text("Scan"),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(Icons.store),
-              title: Text("Mart"),
-            ),
-            ListTile(
-              onTap: () {},
-              leading: Icon(Icons.article),
-              title: Text("Articles"),
-            ),
-            Divider(
-              indent: 20,
-              endIndent: 20,
-              color: blueColor,
+                    builder: (context) => ProfileScreen(),
+                  )),
+              leading: Icon(Icons.person),
+              title: Text("Profile"),
             ),
             ListTile(
               onTap: () {},
@@ -117,6 +105,91 @@ Widget buildMenuItems(BuildContext context) => SingleChildScrollView(
               onTap: () {},
               leading: Icon(Icons.article),
               title: Text("My Articles"),
+            ),
+            ListTile(
+              onTap: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginView(),
+                  ),
+                  (route) => false,
+                );
+              },
+              leading: Icon(Icons.logout),
+              title: Text("Signout"),
+            ),
+            Divider(
+              indent: 20,
+              endIndent: 20,
+              color: blueColor,
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BottomNavBar(initialTabIndex: 0),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.shopping_bag,
+              ),
+              title: Text("Buy"),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BottomNavBar(initialTabIndex: 1),
+                  ),
+                );
+              },
+              leading: Icon(Icons.sell),
+              title: Text("Sell"),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BottomNavBar(initialTabIndex: 2),
+                  ),
+                );
+              },
+              leading: Icon(Icons.camera_alt),
+              title: Text("Scan"),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BottomNavBar(initialTabIndex: 3),
+                  ),
+                );
+              },
+              leading: Icon(Icons.store),
+              title: Text("Mart"),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BottomNavBar(initialTabIndex: 4),
+                  ),
+                );
+              },
+              leading: Icon(Icons.article),
+              title: Text("Articles"),
             ),
           ],
         ),
@@ -143,27 +216,7 @@ class OnboardingScreen extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // Padding(
-                  //   padding: const EdgeInsets.only(left: 15, top: 8),
-                  //   child: Align(
-                  //     alignment: Alignment.topLeft,
-                  //     child: IconButton(
-                  //       onPressed: () {
-                  //         Navigator.push(
-                  //           context,
-                  //           MaterialPageRoute(
-                  //             builder: (context) => ProfileScreen(),
-                  //           ),
-                  //         );
-                  //       },
-                  //       icon: Icon(
-                  //         Icons.settings,
-                  //         color: whiteColor,
-                  //         size: 30,
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
+                  // Your content here
                   Expanded(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
